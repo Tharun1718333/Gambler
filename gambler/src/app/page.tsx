0bfc899c1a0c9ff82b70af670de59aba648695a1
Tharun <reddytharun165@gmail.com>
2024-09-30 10:44:31 +0530

    removed login for deployment

--- gambler/src/app/page.tsx
+++ gambler/src/app/page.tsx
@@ -11,5 +11,5 @@ export default function Home() {
     setIsLoggedIn(status);
   }
 
-  return <div>{isLoggedIn ? <Roll /> : <Login LoginStatus={getStatus} />}</div>;
+  return <div>{true ? <Roll /> : <Login LoginStatus={getStatus} />}</div>;
 }
